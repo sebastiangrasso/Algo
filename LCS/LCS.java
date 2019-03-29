cd82ee7662bf7837808113317e7588faadaa1dd3
sebastiangrasso <33736948+sebastiangrasso@users.noreply.github.com>
2019-03-29 12:26:58 -0700

    Update LCS.java

--- LCS/LCS.java
+++ LCS/LCS.java
@@ -1,6 +1,7 @@
 package lcs;
 
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Set;
 
 public class LCS {
@@ -16,19 +17,59 @@ public class LCS {
     // Shared Helper Methods
     // -----------------------------------------------
     
-    public <Set> collectSolution(String rStr, int r, String cStr, int c, int[][] memo){
-    
-    //base case
-    if (r = 0 || c = 0) {
-    	return { "" };
-    }
-    
-    
-    }
-    
-    // [!] TODO: Add your shared helper methods here!
-    
+    public Set<String> collectSolution(String rStr, int r, String cStr, int c, int[][] memo){
+		
+    	// reached gutters
+		if (r == 0 || c == 0)
+		{
+			Set<String> temp = new HashSet<String>();
+			temp.add("");
+			return temp;
+		}
 
+		// if the characters match
+		if (rStr.charAt(r - 1) == cStr.charAt(c - 1))
+		{
+			// Add the matched letter to all substrings by recursing top and left
+			Set<String> lcs = collectSolution(rStr, r - 1, cStr, c - 1, memo);
+
+			// append current character rStr[r - 1] or cStr[c - 1]
+			// to all LCS of substring rStr[0..r-2] and cStr[0..c-2]
+			for(String sub : lcs) {
+				sub = sub + "" + rStr.charAt(r-1);
+			}
+
+			return lcs;
+		}
+
+		// if characters don't match
+
+		// if top cell of current cell has more value than the left cell,
+		// then ignore current character of String rStr and find all LCS of
+		// substring rStr[0..r-2], cStr[0..c-1]
+		if (memo[r - 1][c] > memo[r][c - 1])
+			return collectSolution(rStr, r - 1, cStr, c, memo);
+
+		// if left cell of current cell has more value than the top cell,
+		// then ignore current character of String cStr and find all LCS of
+		// substring rStr[0..r-1], cStr[0..c-2]
+		if (memo[r][c - 1] > memo[r - 1][c])
+			return collectSolution(rStr, r, cStr, c - 1, memo);
+
+		// if top cell has equal value to the left cell,
+		// then consider both character
+
+		Set<String> top = collectSolution(rStr, r - 1, cStr, c, memo);
+		Set<String> left = collectSolution(rStr, r, cStr, c - 1, memo);
+
+		// merge two Lists and return
+		top.addAll(left);
+
+		return top;
+	}
+	
+
+       
     // -----------------------------------------------
     // Bottom-Up LCS
     // -----------------------------------------------
@@ -69,6 +110,8 @@ public class LCS {
     	int longestSubstring = lcsRecursiveHelper(rStr, rStr.length(), cStr, cStr.length(), memo);
     	memoCheck = memo;
     	
+    	
+    	//for loop to print memo table, shows output is correct
     	for (int[] x : memo)
     	{
     	   for (int y : x)
