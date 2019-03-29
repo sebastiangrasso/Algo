870be2f855fde2cce10d9c5205d9504d44070360
sebastiangrasso <33736948+sebastiangrasso@users.noreply.github.com>
2019-03-29 15:16:04 -0700

    Update LCS.java

--- LCS/LCS.java
+++ LCS/LCS.java
@@ -17,7 +17,7 @@ public class LCS {
     // Shared Helper Methods
     // -----------------------------------------------
     
-    public Set<String> collectSolution(String rStr, int r, String cStr, int c, int[][] memo){
+    public static Set<String> collectSolution(String rStr, int r, String cStr, int c, int[][] memo){
 		
     	// reached gutters
 		if (r == 0 || c == 0)
@@ -32,40 +32,37 @@ public class LCS {
 		{
 			// Add the matched letter to all substrings by recursing top and left
 			Set<String> lcs = collectSolution(rStr, r - 1, cStr, c - 1, memo);
-
+			Set<String> holding = new HashSet<>();
+			
+			String matchingChar = ("" + rStr.charAt(r-1));
+			
 			// append current character rStr[r - 1] or cStr[c - 1]
 			// to all LCS of substring rStr[0..r-2] and cStr[0..c-2]
 			for(String sub : lcs) {
-				sub = sub + "" + rStr.charAt(r-1);
+				sub = sub + matchingChar;
+				holding.add(sub);
 			}
-
-			return lcs;
+			return holding;
 		}
 
 		// if characters don't match
+		
+		Set <String> left = new HashSet<>();
+		Set <String> up = new HashSet<>(); 
 
-		// if top cell of current cell has more value than the left cell,
-		// then ignore current character of String rStr and find all LCS of
-		// substring rStr[0..r-2], cStr[0..c-1]
+		// left vs top
 		if (memo[r - 1][c] > memo[r][c - 1])
-			return collectSolution(rStr, r - 1, cStr, c, memo);
+			left = collectSolution(rStr, r-1, cStr, c, memo);
 
-		// if left cell of current cell has more value than the top cell,
-		// then ignore current character of String cStr and find all LCS of
-		// substring rStr[0..r-1], cStr[0..c-2]
+		// top vs left
 		if (memo[r][c - 1] > memo[r - 1][c])
-			return collectSolution(rStr, r, cStr, c - 1, memo);
+			up = collectSolution(rStr, r, cStr, c-1, memo);
+		
+		//merge two sets
+		left.addAll(up);
+		
 
-		// if top cell has equal value to the left cell,
-		// then consider both character
-
-		Set<String> top = collectSolution(rStr, r - 1, cStr, c, memo);
-		Set<String> left = collectSolution(rStr, r, cStr, c - 1, memo);
-
-		// merge two Lists and return
-		top.addAll(left);
-
-		return top;
+		return left;
 	}
 	
 
@@ -83,13 +80,32 @@ public class LCS {
      * @return The longest common subsequence between rStr and cStr +
      *         [Side Effect] sets memoCheck to refer to table
      */
-    public static Set<String> bottomUpLCS (String rStr, String cStr) {
-        throw new UnsupportedOperationException();
+    public static Set<String> bottomUpLCS (String rStr, String cStr) {    	
+    	int[][] memo = new int [rStr.length()+1][cStr.length()+1];
+
+    	bottomUpTableFill(rStr, cStr, rStr.length(), cStr.length(), memo);
+    	memoCheck = memo;
+
+    	return collectSolution(rStr, rStr.length(), cStr, cStr.length(), memo);
+
     }
-    
-    // [!] TODO: Add any bottom-up specific helpers here!
-    
-    
+
+    static int bottomUpTableFill( String rStr, String cStr, int rInd, int cInd, int memo[][]) { 
+
+    	for (int i=0; i<=rInd; i++) 
+    	{ 
+    		for (int j=0; j<=cInd; j++) 
+    		{ 
+    			if (i == 0 || j == 0) 
+    				memo[i][j] = 0; 
+    			else if (rStr.charAt(rInd - 1) == cStr.charAt(rInd - 1)) 
+          memo[i][j] = 1 + memo[i-1][j-1]; 
+    			else
+          memo[i][j] = Math.max(memo[i-1][j], memo[i][j-1]); 
+    		}
+    	}	
+    	return memo[rInd][cInd];
+  }  
     // -----------------------------------------------
     // Top-Down LCS
     // -----------------------------------------------
@@ -107,20 +123,10 @@ public class LCS {
     	  	 
         int[][] memo = new int [rStr.length()+1][cStr.length()+1];
     
-    	int longestSubstring = lcsRecursiveHelper(rStr, rStr.length(), cStr, cStr.length(), memo);
+    	lcsRecursiveHelper(rStr, rStr.length(), cStr, cStr.length(), memo);
     	memoCheck = memo;
-    	
-    	
-    	//for loop to print memo table, shows output is correct
-    	for (int[] x : memo)
-    	{
-    	   for (int y : x)
-    	   {
-    	        System.out.print(y + " ");
-    	   }
-    	   System.out.println();
-    	}  	
-        throw new UnsupportedOperationException();
+    	  	
+    	return collectSolution(rStr, rStr.length(), cStr, cStr.length(), memo);
     }
     
     /**
@@ -149,7 +155,7 @@ public class LCS {
   
             return memo[rInd][cInd]; 
         } 
-        //if chars don't match, save solution for avoiding future work
+        //if chars don't match, check left and up
         else { 
   
             memo[rInd][cInd] = Math.max(lcsRecursiveHelper(rStr, rInd, cStr, cInd - 1, memo), 
